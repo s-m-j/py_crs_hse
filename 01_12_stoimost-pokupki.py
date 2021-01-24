3af28bf97150dd030fbc7b9c0eac8a34b2728f29
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-01-25 00:28:58 +0300

    Стоимость покупки в рублях и копейках

---
+++ 01_12_stoimost-pokupki.py
@@ -0,0 +1,8 @@
+a = int(input()) # стоимость пирожка рубли
+b = int(input()) # стоимость пирожка копейки
+n = int(input()) # сколько пирожков
+
+total_cost = (a * 100 + b) * n
+r = total_cost // 100
+k = total_cost % 100
+print(r, k)

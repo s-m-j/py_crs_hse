1f7e9aba5bc1eacceb6e1a22ca73f99947aa887e
Sergey Maslov <sergey.s.maslov@gmail.com>
2021-01-24 20:52:55 +0300

    pre-final

--- 01_02_penguins.py
+++ 01_02_penguins.py
@@ -3,14 +3,14 @@
 Программа принмает на вход число n и рисует n пиггвинов
 """
 
-n = int(input())
-line1 = "   _~_   "    
-line2 = "  (o o)  " 
-line3 = " /  V  \ " 
-line4 = "/(  _  )\"
-lene5 = "  ^^ ^^  "
-print(line1 * n, sep=' ')
-print(line2 * n, sep=' ')
-print(line3 * n, sep=' ')
-print(line4 * n, sep=' ')
-print(line5 * n, sep=' ')
+n = input()
+line1 = "   _~_    "    
+line2 = "  (o o)   " 
+line3 = " /  V  \  " 
+line4 = "/(  _  )\ "
+line5 = "  ^^ ^^   "
+print(line1 * int(n))
+print(line2 * int(n))
+print(line3 * int(n))
+print(line4 * int(n))
+print(line5 * int(n))
